8aa8cd6793199fffbfec2a90dd0bbb775566d798
unknown <veladandisoumith3@gmail.com>
2024-11-06 19:05:46 +0530

    total project

---
+++ bck_devops_testing/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Test GET API Endpoint
+app.get('/test', (req, res) => {
+    res.status(200).json({ message: 'API is working correctly!' });
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
